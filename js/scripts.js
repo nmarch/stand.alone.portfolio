287e8599b237dd442723a44fcb7ab2ba065ad463
Nicky March <nickymarch1@hotmail.com>
2016-11-22 14:32:54 -0800

    Version 3

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,15 @@ var numRand = Math.floor(Math.random() * maxRand);
 //console.log(numRand);
 
 // Store images
-var img0 = 'img/fullsize/';
-var img1 = 'img/fullsize/';
-var img2 = 'img/fullsize/';
+var img0 = '/img/fullsize/1.jpg';
+var img1 = '/img/fullsize/2.jpg';
+var img2 = '/img/fullsize/3.jpg';
 
 // Show Images
 $('header img').attr('src', eval('img' + numRand));
+
+// Activate Gallery
+$('figure').lightGallery({
+    selector: 'a'
+    
+});
