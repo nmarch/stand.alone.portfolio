fa5e70a4493b5d4a570c12e1db032dd59669e429
Nicky March <nickymarch1@hotmail.com>
2016-11-22 13:38:21 -0800

    Version 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 // My Scripts
+// Store & Generate Number
+
+var maxRand = 3;
+var numRand = Math.floor(Math.random() * maxRand);
+
+//console.log(numRand);
+
+// Store images
+var img0 = 'img/fullsize/';
+var img1 = 'img/fullsize/';
+var img2 = 'img/fullsize/';
+
+// Show Images
+$('header img').attr('src', eval('img' + numRand));
